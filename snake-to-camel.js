6b28c2713d5115487e597f0610e55b8d2f2acca2
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-10-18 14:55:12 -0500

    Add a function that converts a string from snake case to camel case and a helper function that capitalizes a string

--- snake-to-camel.js
+++ snake-to-camel.js
@@ -1,2 +1,27 @@
-function snakeToCamel() {}
+function snakeToCamel(str) {
+  // split the string and put the words into an array
+  const arrFromStr = str.split('_');
+  // create an array containing all but the first word of the array
+  const arrNotFirst = arrFromStr.slice(1);
+
+  // place the first word into a new array
+  let newStrArr = [arrFromStr[0]];
+
+  for (str of arrNotFirst) {
+    str = capitalize(str);
+    newStrArr.push(str);
+  }
+  newStr = newStrArr.join('');
+  return newStr;
+}
+
+// Helper function that capitalizes a string
+function capitalize(str) {
+  strFirst = str[0].toUpperCase();
+  strRest = str.substring(1);
+  str = strFirst.concat(strRest);
+  return str;
+}
+
+console.log("result1:", snakeToCamel("a_man_a_plan"));
 
