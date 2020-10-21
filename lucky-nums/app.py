3d0770d5fd5761c4b94df8754d82cac8bb9d9f38
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-10-20 20:49:42 -0500

    Enable app to return the correct data when all form fields have valid values

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify
+import requests
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+API_BASE_URL = 'http://numbersapi.com'
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Handle 404 errors by showing custom 404 page."""
+
+    return render_template('404.html'), 404
 
 
 @app.route("/")
@@ -8,3 +24,137 @@ def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+@app.route('/api/get-lucky-num', methods=["POST"])
+def get_lucky_num():
+    """Get trivia based on form input."""
+
+    data = get_form_data()
+    
+    data_json = jsonify(data)
+    
+    # errors = validate_data(data_json)
+
+    # if errors:
+        # return jsonify({errors})
+
+    # res = get_trivia(data_json['year'])
+    res = get_trivia(data['year'])
+
+    return res
+
+
+def request_year_trivia(year):
+    """Return year and fact from Numbers API for given year."""
+
+    res = requests.get(f'{API_BASE_URL}/{year}/year?json')
+    
+    return res
+
+def request_num_trivia():
+    """Return number and trivia from Numbers API for random number."""
+
+    res = requests.get(f'{API_BASE_URL}/random?min=1&max=100&json')
+
+    return res
+
+def get_trivia(year):
+    """Get trivia based on form input.
+    Will run only if there are no errors in data from client."""
+
+    # *************************************************
+    # make API request to Numbers API using those data
+    # *************************************************
+    res_num_trivia = request_num_trivia()
+    data_num=res_num_trivia.json()
+    num_info = {"fact": data_num['text'], "num": data_num['number']}
+
+    # with specific year: res_year_trivia = request_year_trivia(1969)
+    res_year_trivia = request_year_trivia(year)
+    data_year=res_year_trivia.json()
+    year_info = {"fact": data_year['text'], "year": data_year['number']}
+
+    res_json = {"num": num_info, "year": year_info}
+    return res_json
+
+
+def get_form_data():
+    """Get data from client form."""
+
+    # GET FORM INPUT DATA from lucky.js
+
+    data = {}
+    data['name'] = request.json['name']
+    data["year"] = request.json['year']
+    data["email"] = request.json['email']
+    data["color"] = request.json['color']
+
+    return data
+
+# check data for errors
+def validate_data(data):
+    """Check data from client for errors."""
+    valid_colors = ["red", "green", "orange", "blue"]
+    errors = {}
+
+    if not data["name"]:
+        name_err = ["This field is required."]
+        errors['name_err'] = name_err
+    if (not data["year"]) or year < 1900 or year >2000:
+        year_err = ["Invalid value, must be between 1900 and 2000 inclusive is required."]
+        errors['year_err'] = year_err
+    if not data["email"]:
+        email_err = ["This field is required."]
+        errors['email_err'] = email_err
+    if (not data["color"]) or (data["color"] not in valid_colors):
+        color_err = ["Invalid value, must be one of: red, green, orange, blue."]
+        errors['color_err'] = color_err
+
+    return errors
+
+    # send get request to Numbers API and receive response
+
+    # res_json = get_trivia()
+
+    # send the response
+    # return res_json
+
+
+
+
+
+
+
+
+# ***************************************************************
+# *******************
+# ATTEMPTS TO CALL API
+# *******************
+    # res = requests.get(f'{API_BASE_URL}/random?min=1&max=100?json')
+    # res = requests.get(f'{API_BASE_URL}/random?min=1&max=100/trivia?json')
+    # res = requests.get(f'{API_BASE_URL}/random?min=1&max=100/trivia?json')
+
+
+# *******************
+
+
+    # data[text]
+    # data.get[number]
+    # return data
+    # res_json = jsonify(data=data)
+
+
+
+    # res2 = requests.get(f'{API_BASE_URL}/1969/year?json')
+
+    # res = requests.get(f'{API_BASE_URL}', params={'number': 'random', 'type': 'trivia'});
+    # url = f"{API_BASE_URL}"
+
+    # res = requests.get(url, params={'number': 'random', 'type': 'trivia'}, headers={'Content-Type': 'application/json'})
+    # res = jsonify(requests.get(url, params={'number': 'random', 'type': 'trivia'}))
+    # res = requests.get(url, params={'number': 'random', 'type': 'trivia'}).headers["Content-Type"] = "application/json"
+    # res.headers["Content-Type"] = "application/json"
+    # data=res.json()
+    # res_json = jsonify(data=data)
+    # res1 = requests.get(f'{API_BASE_URL}/random/trivia?json')
+    # res2 = requests.get(f'{API_BASE_URL}/1969/year?json')
--- lucky-nums/static/lucky.js
+++ lucky-nums/static/lucky.js
@@ -1,12 +1,82 @@
+// const BASE_URL = "http://127.0.0.1:5000/api/get_lucky_num";
+// const BASE_URL = "http://localhost:5000/api/get_lucky_num";
+const BASE_URL = "/api/get-lucky-num";
+
+const results = document.querySelector('#lucky-results');
+const form = document.querySelector('#lucky-form');
+let nameErr = document.querySelector("#name-err");
+let yearErr = document.querySelector("#year-err");
+let emailErr = document.querySelector("#email-err");
+let colorErr = document.querySelector("#color-err");
+
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+// GIVEN:
+// function processForm(evt) {
+// }
+
+async function processForm(evt) {
+  evt.preventDefault();
+  // get data from form
+  const name = document.querySelector("#name").value;
+  const year = document.querySelector("#year").value;
+  const email = document.querySelector("#email").value;
+  const color = document.querySelector("#color").value;
+  console.log("form values: ", name, year, email, color);
+
+  const formData = {name, year, email, color};
+  // const formData = {"name": name, "year": year, "email": email, "color": color};
+  // const formData = {name: name, year: year, email: email, color: color};
+  console.log("formData: ", formData);
+
+  // make AJAX call to our Flask API
+  const res = await axios.post(`${BASE_URL}`, formData);
+
+  console.log("post request result: ", res);
+
+  return res;
 }
+
+form.addEventListener("submit", async function(evt) {
+  res = await processForm(evt); // response from Flask backend API
+  handleResponse(res);
+})
+
+
+// GIVEN:
+// function handleResponse(resp) {
+// }
+
 
 /** handleResponse: deal with response from our lucky-num API. */
+function handleResponse(res) {
+  if (res["errors"]) {
+    nameErr.innerText = res["errors"]["name"][0] || '';
+    console.log("I looked at 'name'!");
+    yearErr.innerText = res["errors"]["year"][0] || '';
+    emailErr.innerText = res["errors"]["email"][0] || '';
+    colorErr.innerText = res["errors"]["color"][0] || '';
+  }
+    // if (res["errors"]["name"]) {
+  //   nameErr.innerText = res["errors"]["name"][0];
+  // }
 
-function handleResponse(resp) {
+  // if (res["errors"]["year"]) {
+  //   yearErr.innerText = res["errors"]["year"][0];
+  // }
+
+  // if (res["errors"]["email"]) {
+  //   emailErr.innerText = res["errors"]["email"][0];
+  // }
+
+  // if (res["errors"]["color"]) {
+  //   colorErr.innerText = res["errors"]["color"][0];
+  // }
+  console.log("LINE 75 res: ", res);
+  if (!res["errors"]) {
+    results.innerHTML = `<p>Your lucky number is ${res["data"]["num"]["num"]} (${res["data"]["num"]["fact"]}).</p><p>Your birth year (${res["data"]["year"]["year"]}) fact is ${res["data"]["year"]["fact"]}</p>`
+  }
 }
-
-
-$("#lucky-form").on("submit", processForm);
+// how to get the result variable out?
+// GIVEN:
+// $("#lucky-form").on("submit", processForm);
